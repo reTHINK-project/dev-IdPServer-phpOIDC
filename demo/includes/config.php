f6b702b40a01cd146d9e962bde60ffa113a6b5e6
Simon Becot <simon.becot@orange.com>
2017-03-16 11:38:09 +0100

    Corrections

--- demo/includes/config.php
+++ demo/includes/config.php
@@ -20,15 +20,11 @@ date_default_timezone_set("Europe/Paris");
  * These are the database login details
  */
 
-$wgDBserver         = $_ENV['MYSQL_HOST'];
-$wgDBport 	    = $_ENV['MYSQL_PORT'];
-$wgDBname           = $_ENV['MYSQL_DATABASE'];
-$wgDBuser           = $_ENV['MYSQL_USER'];
-$wgDBpassword       = $_ENV['MYSQL_PASSWORD'];
 
-define('HOST', $wgDBserver);     // The host you want to connect to.
-define('USER', $wgDBuser);    // The database username.
-define('PASSWORD', $wgDBpassword);    // The database password.
+
+define('HOST',               'MYSQL_HOST');
+define('USER',               'MYSQL_USER');
+define('PASSWORD',           'MYSQL_PASSWORD');
 define('DATABASE', 'demo');    // The database name.
 
 define('CAN_REGISTER', 'any');
@@ -41,8 +37,8 @@ define('SECURE', 'false');
 */
 define("SP_PATH", '/' . basename(dirname($_SERVER['SCRIPT_FILENAME'])));
 
-$SP_URL="https://oidc-ns.kermit.orange-labs.fr/demo";
-$IDP_URL="https://oidc-ns.kermit.orange-labs.fr/phpOp/index.php";
+$SP_URL="https://".$_SERVER["HTTP_X_FORWARDED_SERVER"]."/demo";
+$IDP_URL="https://".$_SERVER["HTTP_X_FORWARDED_SERVER"]."/phpOp/index.php";
 
 $REDIRECT_URI=$SP_URL."/demoback.php";
 
--- start.sh
+++ start.sh
@@ -7,6 +7,10 @@ sed -i "s/MYSQL_DATABASE/"$MYSQL_DATABASE"/"  /var/www/html/phpOp/dbconf.php
 
 cp /var/www/html/phpOp/dbconf.php /var/www/html/phpRp/dbconf.php
 
+sed -i "s/MYSQL_USER/"$MYSQL_USER"/"  /var/www/html/demo/includes/config.php
+sed -i "s/MYSQL_PASSWORD/"$MYSQL_PASSWORD"/"  /var/www/html/demo/includes/config.php
+sed -i "s/MYSQL_HOST/"$MYSQL_HOST"/"  /var/www/html/demo/includes/config.php
+
 service apache2 restart
 
 tail -f /dev/null
